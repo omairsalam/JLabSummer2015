24b3b7e2def08a2d7048de41664d0ae26fcae501
omairsalam <omair.alam@richmond.edu>
2015-06-18 10:46:41 -0400

    trying addition the other way

--- targetPractice2.pl
+++ targetPractice2.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-#my name is Omair Shahzad Alam and I am testing different systems
+#my name is Omair Shahzad Alam and I am testing different systems and this is really cool
 # loading GEMC geometry routines
 use strict;
 use lib ("$ENV{GEMC}/io");
